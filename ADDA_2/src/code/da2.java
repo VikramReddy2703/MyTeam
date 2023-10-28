28685df675198b4ea43b9699aec27758dd5ada53
revan <revan@DESKTOP-1TO1UH6>
2023-10-28 18:20:12 +0530

    developer1 commit

--- ADDA_2/src/code/da2.java
+++ ADDA_2/src/code/da2.java
@@ -1,6 +1,6 @@
 package code;
 public class da2{
 	public static void main (String[] args) {
-		System.out.println("Hi This is Team Lead!!!");
+		System.out.println("Hi This is Team developer1!!!");
 	}
 }
